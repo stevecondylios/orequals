5bfec1b7a010dc309d3cefbd9c78e4cb36247204
Steve Condylios <steve.condylios@gmail.com>
2022-03-26 20:56:18 +1100

    Add append

---
+++ append.R
@@ -0,0 +1,33 @@
+
+
+
+# Demonstrating class methods E.g. print having a different effect on an object of class xml_nodeset
+# than for object of class string. 
+
+# Ruby's << would be handy when creating the 'titles' object
+# It could be much more expressive, terse, readable and elegant if << were possible in R
+# Q: can we write such a function? 
+
+library(tidyverse)
+library(rvest)
+
+titles <- ""; for(i in 1:25){ a <- paste0(a, paste0("<h1>Title ", i, "</h1>"))}
+
+web <- paste0("<html><head></head><body><",
+              titles,
+              "</body></html>")
+
+web %>% 
+  read_html %>% 
+  html_nodes("h1") %>% 
+  print
+
+something <- rep("hi there", 25)
+
+print(something)
+
+
+
+
+
+
