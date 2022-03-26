fa9841d7c336435ebef72cce76010fac04a64feb
Steve Condylios <steve.condylios@gmail.com>
2022-03-26 20:59:25 +1100

    Add link to slice() class method

--- append.R
+++ append.R
@@ -3,6 +3,7 @@
 
 # Demonstrating class methods E.g. print having a different effect on an object of class xml_nodeset
 # than for object of class string. 
+# Another example of class method (slice()): https://stackoverflow.com/a/59221761/
 
 # Ruby's << would be handy when creating the 'titles' object
 # It could be much more expressive, terse, readable and elegant if << were possible in R
